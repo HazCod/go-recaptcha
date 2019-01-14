84ba414a97648e6657b360031d0aa6edfb0609d6
HazCod <hazcod@users.noreply.github.com>
2019-01-14 10:50:33 +0100

    Refactor to recaptcha v3 usage

--- recaptcha.go
+++ recaptcha.go
@@ -1,20 +1,18 @@
-// Package recaptcha handles reCaptcha (http://www.google.com/recaptcha) form submissions
-//
-// This package is designed to be called from within an HTTP server or web framework
-// which offers reCaptcha form inputs and requires them to be evaluated for correctness
-//
-
+// This package verifies reCaptcha v3 (http://www.google.com/recaptcha) responses
 package recaptcha
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
+// google recaptcha response
 type RecaptchaResponse struct {
 	Success     bool      `json:"success"`
 	Score       float64   `json:"score"`
@@ -24,17 +22,17 @@ type RecaptchaResponse struct {
 	ErrorCodes  []string  `json:"error-codes"`
 }
 
+// recaptcha api endpoint
 const recaptchaServer = "https://www.google.com/recaptcha/api/siteverify"
 
+// main object
 type Recaptcha struct {
 	PrivateKey string
 }
 
-// check uses the client ip address, the challenge code from the reCaptcha form,
-// and the client's response input to that challenge to determine whether or not
-// the client answered the reCaptcha input question correctly.
-// It returns a boolean value indicating whether or not the client answered correctly.
+// check : initiate a recaptcha verify request
 func (r *Recaptcha) check(remoteAddr net.IP, captchaResponse string) (RecaptchaResponse, error) {
+	// fire off request
 	resp, err := http.PostForm(
 		recaptchaServer,
 		url.Values{
@@ -44,33 +42,49 @@ func (r *Recaptcha) check(remoteAddr net.IP, captchaResponse string) (RecaptchaR
 		},
 	)
 
+	// request failed
 	if err != nil {
 		return RecaptchaResponse{Success: false}, err
 	}
 
+	// close response when function exits
 	defer resp.Body.Close()
 
+	// read response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return RecaptchaResponse{Success: false}, err
 	}
 
+	// parse json to our response object
 	var response RecaptchaResponse
 	err = json.Unmarshal(body, &response)
 	if err != nil {
 		return RecaptchaResponse{Success: false}, err
 	}
 
+	// return our object response
 	return response, nil
 }
 
-// Confirm is the public interface function.
-// It calls check, which the client ip address, the challenge code from the reCaptcha form,
-// and the client's response input to that challenge to determine whether or not
-// the client answered the reCaptcha input question correctly.
-// It returns a boolean value indicating whether or not the client answered correctly.
-func (r *Recaptcha) Confirm(remoteip net.IP, response string) (result bool, err error) {
+// Verify : check user IP, captcha subject (= page) and captcha response
+func (r *Recaptcha) Verify(remoteip net.IP, action string, response string, minScore uint) (success bool, err error) {
 	resp, err := r.check(remoteip, response)
-	result = resp.Success
-	return
+	// fetch/parsing failed
+	if err != nil {
+		return false, err
+	}
+
+	// captcha subject did not match
+	if strings.ToLower(resp.Action) != strings.ToLower(action) {
+		return false, errors.New("recaptcha actions do not match")
+	}
+
+	// recaptcha token was not valid
+	if !resp.Success {
+		return false, nil
+	}
+
+	// user treshold was not enough
+	return uint(resp.Score) >= minScore, nil
 }
